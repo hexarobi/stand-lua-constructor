1994f4a8c525609af1a78c8e1a63719fcc3f2f20
Tyler Chamberlain <hexarobi@gmail.com>
2022-12-15 16:57:13 -0600

    bump versions

--- lib/constructor/convertors.lua
+++ lib/constructor/convertors.lua
@@ -1,7 +1,7 @@
 -- Construct Convertors
 -- Transforms various file formats into Construct format
 
-local SCRIPT_VERSION = "0.31b2"
+local SCRIPT_VERSION = "0.31"
 local convertor = {
     SCRIPT_VERSION = SCRIPT_VERSION
 }
