c4471fa991107d96d191b5c95a3efa4a5449a502
Tyler Chamberlain <hexarobi@gmail.com>
2022-12-17 08:27:52 -0600

    fix bug with nested curated attachment

--- Constructor.lua
+++ Constructor.lua
@@ -4,7 +4,7 @@
 -- Allows for constructing custom vehicles and maps
 -- https://github.com/hexarobi/stand-lua-constructor
 
-local SCRIPT_VERSION = "0.32b3"
+local SCRIPT_VERSION = "0.32b4"
 local AUTO_UPDATE_BRANCHES = {
     { "main", {}, "More stable, but updated less often.", "main", },
     { "dev", {}, "Cutting edge updates, but less stable.", "dev", },
@@ -1298,14 +1298,16 @@ local function build_curated_attachments_menu(attachment, root_menu, curated_ite
             end
         end)
     else
-        curated_item.load_menu = menu.action(root_menu, curated_item.name or "Unknown", {}, "", function()
-            local child_attachment = copy_construct_plan(curated_item)
-            child_attachment.root = attachment.root
-            child_attachment.parent = attachment
-            build_construct_from_plan(child_attachment)
-        end)
-        menu.on_focus(curated_item.load_menu, function(direction) if direction ~= 0 then add_preview(curated_item) end end)
-        menu.on_blur(curated_item.load_menu, function(direction) if direction ~= 0 then remove_preview() end end)
+        if curated_item.load_menu == nil then
+            curated_item.load_menu = menu.action(root_menu, curated_item.name or "Unknown", {}, "", function()
+                local child_attachment = copy_construct_plan(curated_item)
+                child_attachment.root = attachment.root
+                child_attachment.parent = attachment
+                build_construct_from_plan(child_attachment)
+            end)
+            menu.on_focus(curated_item.load_menu, function(direction) if direction ~= 0 then add_preview(curated_item) end end)
+            menu.on_blur(curated_item.load_menu, function(direction) if direction ~= 0 then remove_preview() end end)
+        end
     end
 end
 
@@ -1949,7 +1951,7 @@ constructor.add_attachment_add_attachment_options = function(attachment)
 
         if attachment.menus.curated_attachments ~= nil then return end
         attachment.menus.curated_attachments = menu.list(attachment.menus.add_attachment, t("Curated"), {}, t("Browse a curated collection of attachments"))
-        for _, curated_item in pairs(curated_attachments) do
+        for _, curated_item in pairs(constructor_lib.table_copy(curated_attachments)) do
             build_curated_attachments_menu(attachment, attachment.menus.curated_attachments, curated_item)
         end
 
