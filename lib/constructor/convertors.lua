04f71ab6fb241d45d6135c435a8b77ae43824d2b
Tyler Chamberlain <hexarobi@gmail.com>
2023-05-21 04:59:28 -0500

    Update convertors.lua

--- lib/constructor/convertors.lua
+++ lib/constructor/convertors.lua
@@ -1,7 +1,7 @@
 -- Construct Convertors
 -- Transforms various file formats into Construct format
 
-local SCRIPT_VERSION = "0.37"
+local SCRIPT_VERSION = "0.38b1"
 local convertor = {
     SCRIPT_VERSION = SCRIPT_VERSION
 }
