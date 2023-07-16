f2bb8ae9dfd2ff4e8868eb0138bc247d38a59e3e
Tyler Chamberlain <hexarobi@gmail.com>
2023-07-16 12:08:06 -0500

    Update browser.lua

--- lib/constructor/browser.lua
+++ lib/constructor/browser.lua
@@ -1,5 +1,5 @@
 --- Browser Lib
---- 0.2
+--- 0.3
 --- by Hexarobi
 ---
 --- Add browsable and searchable hierarchical menu structures to Stand
@@ -26,15 +26,13 @@
 ---   },
 ---   ...
 --- }
---- browser.browse_item(menu.my_root(), vehicles_items, {
----    add_item_menu=function(root_menu, item)
----        root_menu:action(item.name, {}, "", function()
----          util.toast(item.name)
----        end)
----    end
---- })
+--- browser.browse_item(menu.my_root(), vehicles_items, function(parent_menu, item)
+---     parent_menu:action(item.name, {}, "", function()
+---       util.toast(item.name)
+---     end)
+--- end)
 
---local inspect = require("inspect")
+local inspect = require("inspect")
 
 local browser = {}
 local config = {
@@ -57,21 +55,27 @@ browser.search = function(search_params)
     if search_params.menus == nil then search_params.menus = {} end
     if search_params.results == nil then search_params.results = {} end
     local results = search_params.query_function(search_params)
-    for i = (search_params.page_size*search_params.page_number)+1, search_params.page_size*(search_params.page_number+1) do
+    local more_results_available = false
+    local first_result_index = (search_params.page_size*search_params.page_number)+1
+    local last_result_index = search_params.page_size*(search_params.page_number+1)
+    for i = first_result_index, last_result_index do
         if results[i] then
             local search_result_menu = search_params.add_item_menu_function(search_params, results[i])
             table.insert(search_params.results, search_result_menu)
         end
+        more_results_available = (results[i+1] ~= nil)
     end
     if search_params.menus.search_add_more ~= nil and search_params.menus.search_add_more:isValid() then
         menu.delete(search_params.menus.search_add_more)
     end
-    search_params.menus.search_add_more = menu.action(search_params.menus.root, "Load More", {}, "", function()
-        local more_search_params = search_params
-        more_search_params.page_number = more_search_params.page_number + 1
-        browser.search(more_search_params)
-    end)
-    table.insert(search_params.results, search_params.menus.search_add_more)
+    if more_results_available then
+        search_params.menus.search_add_more = menu.action(search_params.menus.root, "[More]", {}, "", function()
+            local more_search_params = search_params
+            more_search_params.page_number = more_search_params.page_number + 1
+            browser.search(more_search_params)
+        end)
+        table.insert(search_params.results, search_params.menus.search_add_more)
+    end
 end
 
 local function delete_menus(menus)
@@ -89,17 +93,22 @@ browser.search_items = function(folder, query, results)
         if item.items ~= nil then
             browser.search_items(item, query, results)
         else
-            if string.match(item.name:lower(), query:lower()) then
-                table.insert(results, item)
+            if type(item.name) == "string" then
+                if string.match(item.name:lower(), query:lower()) then
+                    table.insert(results, item)
+                end
+            else
+                util.log("Warning: Item skipped from search due to invalid name field "..inspect(item))
             end
         end
     end
     return results
 end
 
-browser.browse_item = function(root_menu, root_item, add_item_menu_function)
-    if root_item.items ~= nil then
-        local menu_list = root_menu:list(root_item.name)
+browser.browse_item = function(parent_menu, this_item, add_item_menu_function, browse_params)
+    if browse_params == nil then browse_params = {} end
+    if this_item.items ~= nil then
+        local menu_list = parent_menu:list(this_item.name)
         state.search_menu_counter = state.search_menu_counter + 1
         local search_command = "search"..state.search_menu_counter
         local search_menu = menu_list:list("Search", {}, "", function() menu.show_command_box(search_command.." ") end)
@@ -107,7 +116,7 @@ browser.browse_item = function(root_menu, root_item, add_item_menu_function)
             delete_menus(state.search_results_menus)
             state.search_results_menus = {}
             browser.search({
-                root_item=root_item,
+                this_item=this_item,
                 query=query,
                 results=state.search_results_menus,
                 menus={
@@ -117,7 +126,7 @@ browser.browse_item = function(root_menu, root_item, add_item_menu_function)
                     if browse_params.query_function ~= nil then
                         return browse_params.query_function(search_params)
                     else
-                        return browser.search_items(search_params.root_item, search_params.query)
+                        return browser.search_items(search_params.this_item, search_params.query)
                     end
                 end,
                 add_item_menu_function=function(search_params, item)
@@ -128,10 +137,10 @@ browser.browse_item = function(root_menu, root_item, add_item_menu_function)
             })
         end)
         --if browse_params.additional_page_menus ~= nil then
-        --    browse_params.additional_page_menus(browse_params, root_menu)
+        --    browse_params.additional_page_menus(browse_params, parent_menu)
         --end
         menu_list:divider("Browse")
-        for _, item in pairs(root_item.items) do
+        for _, item in pairs(this_item.items) do
             if type(item) == "table" then
                 if item.items ~= nil then
                     browser.browse_item(menu_list, item, add_item_menu_function)
