00cdc98283a7199bc136a27fbeb1bc49149a4f2d
Tyler Chamberlain <hexarobi@gmail.com>
2023-11-19 17:02:50 -0600

    Update Constructor.lua

--- Constructor.lua
+++ Constructor.lua
@@ -4,7 +4,7 @@
 -- Allows for constructing custom vehicles and maps
 -- https://github.com/hexarobi/stand-lua-constructor
 
-local SCRIPT_VERSION = "0.42r"
+local SCRIPT_VERSION = "0.42.1r"
 
 ---
 --- Config
