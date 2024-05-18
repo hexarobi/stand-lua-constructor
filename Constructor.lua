fbd55597df5832d0353b437d46765033613c45d7
Tyler Chamberlain <hexarobi@gmail.com>
2024-05-18 18:37:31 -0500

    Update Constructor.lua

--- Constructor.lua
+++ Constructor.lua
@@ -4,7 +4,7 @@
 -- Allows for constructing custom vehicles and maps
 -- https://github.com/hexarobi/stand-lua-constructor
 
-local SCRIPT_VERSION = "0.50b1"
+local SCRIPT_VERSION = "0.50b2"
 local AUTO_UPDATE_BRANCHES = {
     { "main", {}, "More stable, but updated less often.", "main", },
     { "dev", {}, "Cutting edge updates, but less stable.", "dev", },
@@ -973,7 +973,9 @@ end
 local function draw_editing_bounding_box(attachment)
     if attachment.is_editing and menu.is_open() then
         --debug_log("Drawing bounding box tick "..attachment.name)
-        constructor_lib.draw_bounding_box(attachment.handle, config.preview_bounding_box_color)
+        if attachment.type ~= "PARTICLE" then
+            constructor_lib.draw_bounding_box(attachment.handle, config.preview_bounding_box_color)
+        end
     end
     for _, child_attachment in pairs(attachment.children) do
         if child_attachment == attachment then error("Invalid child attachment") end
